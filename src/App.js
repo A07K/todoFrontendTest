e52350ec46bf7d4f81266c0e951834f60a524674
A07K <kushwaha07adity@gmail.com>
2025-04-14 12:14:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/tasks")
+      .then((res) => res.json())
+      .then((data) => setTasks(data));
+  }, []);
+
+  const addTask = () => {
+    if (task.trim()) {
+      fetch("http://localhost:3001/tasks", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ task }),
+      }).then(() => {
+        setTasks([...tasks, task]);
+        setTask("");
+      });
+    }
+  };
+
+  const removeTask = (index) => {
+    const newTasks = tasks.filter((_, i) => i !== index);
+    setTasks(newTasks);
+
+    fetch(`http://localhost:3001/tasks/${index}`, {
+      method: "DELETE",
+    }).then(() => {
+      const newTasks = tasks.filter((_, i) => i !== index);
+      setTasks(newTasks);
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To-do</h1>
+      <input
+        text="text"
+        value={task}
+        placeholder="Add a task"
+        onChange={(e) => setTask(e.target.value)}
+      ></input>
+      <button onClick={addTask}>Add the task</button>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task}
+            <button onClick={() => removeTask(index)}>-</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
